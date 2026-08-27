5691b05ff5c703d87e3bce18425dded6031c16cb
marianarosa31-pixel <mariana.rosa31@escola.pr.gov.br>
2026-08-27 12:18:33 -0300

    dfgh

---
+++ scrip.js
@@ -0,0 +1,26 @@
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPerguntas = document.querySelector(".caixa-perguntas");
+const caixaAlternativa = document.querySelector(".caixa-alternativa");
+const caixaResultado = document.querySelector(".caixa-resultado");
+const caixaResultdo = document.querySelector(".texto-resltado");
+
+const perguntas = [
+    "Pergunta 1",
+    "Pergunta 2"
+];
+const perguntas = [
+    {
+        enunciado: "Pergunta 1",
+        alternativas: [
+            "Alternativa 1",
+            "Alternativa 2"
+        ]
+    },
+    {
+        enunciado: "Pergunta 2",
+        alternativas: [
+            "Alternativa 1",
+            "Alternativa 2"
+        ]
+    }
+]; 
